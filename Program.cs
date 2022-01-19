15becb6a0a16273e9987fb1caa5fe89c6b700127
sarahebrooks12 <sarahebrooks12@gmail.com>
2022-01-19 10:39:51 -0500

    intro

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace Intro
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("What's your name?");
+            Console.ReadLine();
+            Console.WriteLine("What is your name?");
+            string Tony = Console.ReadLine();
+            Console.WriteLine($"Fuck you, {Tony}");
+            Console.WriteLine($"What's your name?");
+            string Ezekiel = Console.ReadLine();
+            Console.WriteLine($"Fuck you, {Ezekiel}");
+            
+        }
+    }
+}
